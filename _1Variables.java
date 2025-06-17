90783060a41d1f4757351aad8b25ae793fda4ea8
SaidC1 <txpro123@hotmail.com>
2025-06-16 20:30:31 -0600

    Add files via upload

---
+++ _1Variables.java
@@ -0,0 +1,17 @@
+package ejercicios;
+
+public class _1Variables {
+    public static void main(String[] args) {
+        String nombre = "Said";
+        int edad = 26;
+        double estatura = 1.64;
+        boolean estudiando = true;
+        char letrafavorita = 'S';
+
+        System.out.println("Tu nombre es: " + nombre);
+        System.out.println("Tu edad es: " + edad);
+        System.out.println("Tu estatura es de: " + estatura);
+        System.out.println("Estás estudiando actualmente: " + estudiando);
+        System.out.println("Tu letra favorita es: " + letrafavorita);
+    }
+}
