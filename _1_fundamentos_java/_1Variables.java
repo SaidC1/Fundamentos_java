9816ee42c07e05c6aee35f857ba5505c4d154f80
Irvin Said <txpro123@hotmail.com>
2025-06-19 22:43:21 -0600

    Add files via upload

---
+++ _1_fundamentos_java/_1Variables.java
@@ -0,0 +1,18 @@
+package java_mini_proyectos._1_fundamentos_java;
+
+public class _1Variables {
+    public static void main(String[] args) {
+        String nombre = "Said";
+        int edad = 26;
+        double estatura = 1.64;
+        boolean estudiando = true;
+        char letraFavorita = 'S';
+
+        System.out.println("Tu nombre es: " + nombre);
+        System.out.println("Tu edad es: " + edad);
+        System.out.println("Tu altura es: " + estatura);
+        System.out.println("Tu estas estudiando: " + estudiando);
+        System.out.println("Tu letra favorita es: " + letraFavorita);
+
+    }
+}
