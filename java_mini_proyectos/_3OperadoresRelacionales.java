e7fa6dc841f351533546990d3e0de5f5e6e721f5
Irvin Said <txpro123@hotmail.com>
2025-06-19 21:52:55 -0600

    Add files via upload

---
+++ java_mini_proyectos/_3OperadoresRelacionales.java
@@ -0,0 +1,19 @@
+package java_mini_proyectos;
+
+public class _3OperadoresRelacionales {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 3;
+
+        System.out.println("Suma: " + (a + b));
+        System.out.println("Suma: " + (a + b));
+        System.out.println("Multiplicación: " + (a * b));
+        System.out.println("División: " + (a / b));
+        System.out.println("Residuo:" + (a % b));
+
+        System.out.println("a > b: " + (a > b));
+        System.out.println("a == b: " + (a == b));
+        System.out.println("a >= b: " + (a >= b));
+        System.out.println("a > 0 y b > 0: " + (a > 0 && b > 0));
+    }
+}
