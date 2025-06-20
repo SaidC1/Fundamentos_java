c472995debdc406c3f1750ee96840f640eb5831a
Irvin Said <txpro123@hotmail.com>
2025-06-19 21:58:54 -0600

    Add files via upload

---
+++ _2Condicionales.java
@@ -0,0 +1,18 @@
+package java_mini_proyectos;
+
+public class _2Condicionales {
+    public static void main(String[] args) {
+        int nota = 85;
+
+        if(nota >= 90) {
+            System.out.println("Excelente");
+        }else if (nota >= 70) { 
+            System.out.println("Bien");
+        }else if (nota >= 60) {
+            System.out.println("Suficiente");
+        }else {
+            System.out.println("Reprobado");
+        }
+    }
+    
+}
---
+++ _6BucleAvanzado.java
@@ -0,0 +1,24 @@
+package java_mini_proyectos;
+
+public class _6BucleAvanzado {
+    public static void main(String[] args) {
+        //Mostrar números del 7 al 10 usando for
+        for(int i = 7; i <= 10; i++) {
+            System.out.println("For: " + i);
+        }
+
+        //Mostrar números del 7 al 10 usando while
+        int j = 7;
+        while(j <= 10) {
+            System.out.println("While: " + j);
+            j++;
+        }
+
+        // do-while que se ejecuta al menos una vez
+        int k = 7;
+        do {
+            System.out.println("Do-While: " + k);
+            k++;
+        }while (k <= 10);
+    }
+}
